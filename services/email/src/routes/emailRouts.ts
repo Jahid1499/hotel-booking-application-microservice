39de1524549e84d3068d956451a1c9e872e2aabe
jahid <jahid15-1499@diu.edu.bd>
2024-04-10 00:17:54 +0600

    project setup for email service and docker compose file setup for mailhog image

---
+++ services/email/src/routes/emailRouts.ts
@@ -0,0 +1,10 @@
+
+// import { userRegistration, verifyToken } from '@/controller';
+// import userLogin from '@/controller/userLogin';
+import express from 'express';
+const emailRouter = express.Router();
+
+// emailRouter.post('/registration', userRegistration)
+
+
+export default emailRouter;
