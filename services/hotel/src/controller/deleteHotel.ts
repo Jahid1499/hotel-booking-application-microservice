d35f8fa3f834e54029cd842868fa476468a2da02
jahid <jahid15-1499@diu.edu.bd>
2024-04-20 00:57:14 +0600

    delete hotel form hotel service, delete logic done

--- services/hotel/src/controller/deleteHotel.ts
+++ services/hotel/src/controller/deleteHotel.ts
@@ -1,9 +1,19 @@
+import prisma from '@/prisma';
 import { NextFunction, Request, Response } from 'express';
 
 const deleteHotel = async (req: Request, res: Response, next: NextFunction) => {
     try {
 
+        const id = req.params.id;
 
+        await prisma.hotel.delete({
+            where: {
+                id: id
+            }
+        });
+
+
+        res.status(200).json({ message: 'Hotel successfully deleted' });
     } catch (error) {
         next(error);
     }
