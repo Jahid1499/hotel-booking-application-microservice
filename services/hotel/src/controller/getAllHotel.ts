207e6cba51cab1e0eaea078285a3ba95ef95fe30
jahid <jahid15-1499@diu.edu.bd>
2024-04-20 01:18:58 +0600

    get all hotel by auth id, logic done

--- services/hotel/src/controller/getAllHotel.ts
+++ services/hotel/src/controller/getAllHotel.ts
@@ -1,8 +1,27 @@
+import prisma from '@/prisma';
 import { NextFunction, Request, Response } from 'express';
 
 const getAllHotel = async (req: Request, res: Response, next: NextFunction) => {
     try {
 
+        const userId = (req.headers['x-user-id'] as string) || null;
+
+        if (!userId) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // find hotel by id
+        const hotel = await prisma.hotel.findMany({
+            where: {
+                userId
+            },
+            include: {
+                images: true,
+                facilities: true
+            }
+        })
+
+        return res.status(200).json({ status: 'success', hotel });
 
     } catch (error) {
         next(error);
