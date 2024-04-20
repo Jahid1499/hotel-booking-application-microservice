189fd7f62c4c0780618443e7e58f8d11e133c987
jahid <jahid15-1499@diu.edu.bd>
2024-04-21 01:05:20 +0600

    project setup for booking service

---
+++ services/booking/src/schemas.ts
@@ -0,0 +1,23 @@
+
+
+// export const HotelCreateSchema = z.object({
+//     userId: z.string(),
+//     name: z.string(),
+//     address: z.string(),
+//     city: z.string(),
+//     country: z.string(),
+//     description: z.string(),
+//     adult_count: z.number().min(1),
+//     child_count: z.number().min(0).default(0),
+//     price_per_night: z.number(),
+//     start_rating: z.number().min(1).max(5),
+//     type: z.nativeEnum(Type)
+// });
+
+// export const HotelUpdateSchema = HotelCreateSchema.omit({ userId: true }).partial();
+
+
+// export const FacilitiesSchema = z.object({
+//     facilities: z.string(),
+//     hotel_id: z.string(),
+// });
